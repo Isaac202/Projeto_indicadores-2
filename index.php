296c27043a15f820e0497b3b7a808754f013dcd1
Isaac de Oliveira <isaacbyguaracity@gmail.com>
2020-10-13 16:58:57 -0300

    Layout basico criado

--- index.php
+++ index.php
@@ -2,12 +2,85 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximun-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <title>Ciencia da Informação</title>
 </head>
 <body>
-    
+    <section class="topo">
+        <div class='center'>
+            <header>
+                <div class='logo'><img src="imagens/logo_portal.png" alt=""></div>
+            </header>
+            <ul class='menu-desktop'>
+                <li><a href= /a>HOME</li>
+                <li><a href= /a>SOBRE</li>
+                <li><a href= /a>CONTATO</li>
+            </ul>
+           <!-- <div class='clear'></div> -->
+        </div><!--center-->
+    </section> <!--Topo-->
+    <section class='meio'>
+        <div class= 'banner'>
+         
+        </div><!--banner -->
+
+        <div class='conteudo'>
+            <div class='sobre'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim in lorem at eleifend. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse potenti. Proin dapibus vestibulum ligula ac molestie. Maecenas at velit eget erat consequat bibendum sed non risus. Phasellus nec velit aliquam, viverra augue a, molestie turpis. Fusce vel lorem nec diam suscipit scelerisque ac et dui. Vestibulum id nibh sit amet quam feugiat hendrerit. Vivamus eget ipsum nec sapien feugiat ullamcorper. Cras pharetra dui a eros pellentesque ultrices eget ac turpis. Nullam id justo viverra, lacinia lacus efficitur, euismod libero. Fusce ullamcorper ac velit at lobortis.
+
+Nunc posuere mauris nec sapien egestas, eu rhoncus nisi varius. Nullam nec nisi augue. Duis eu nisl fringilla massa gravida scelerisque vel eget sem. Sed ornare at sapien quis rhoncus. Nam vel velit non justo dapibus vulputate et in mi. Nam sit amet porta nisl. In finibus vel dui vel tristique.
+
+In hac habitasse platea dictumst. Etiam pharetra eros nec luctus ullamcorper. Nulla fermentum libero sit amet urna tincidunt pulvinar. Aliquam tristique magna lectus, eu vulputate augue aliquam eget. Aenean et facilisis est, et convallis sapien. Quisque malesuada nunc nisl. Duis porttitor lectus eget elit facilisis, sit amet eleifend nisi tincidunt. Aliquam eu est massa. Cras vulputate hendrerit pharetra. Aliquam egestas nulla ligula, ut dictum velit dictum aliquam. Fusce congue consequat risus, non sollicitudin nisl volutpat at. Phasellus vestibulum, orci lacinia blandit sodales, massa diam tempor augue, nec porta neque nibh ac augue.
+
+In egestas cursus rhoncus. Nulla ac feugiat sem, et vulputate mauris. Curabitur eleifend, velit et dictum mollis, purus nisl molestie velit, sed sollicitudin diam urna id quam. Mauris elementum maximus vestibulum. Cras congue nunc sollicitudin ex dictum semper. Vestibulum risus sapien, hendrerit tempus interdum semper, varius ac erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi varius faucibus tortor, sit amet iaculis tellus vulputate commodo. Suspendisse potenti. Donec est tellus, feugiat ut neque eget, laoreet dapibus arcu. Phasellus ac dolor feugiat, ullamcorper nibh luctus, vehicula ipsum. Suspendisse sit amet nunc congue, imperdiet risus nec, interdum quam. Maecenas vitae sapien tortor.
+
+Suspendisse fermentum felis eget metus suscipit, quis semper sapien tincidunt. In a urna dolor. Donec ac porttitor quam. Phasellus lacinia sapien sed justo fermentum, in euismod augue dignissim. Quisque elementum consectetur tortor in fringilla. Suspendisse laoreet est nec metus scelerisque mollis. Morbi congue et dui tincidunt commodo. Curabitur finibus aliquam venenatis. Integer non malesuada sapien. Suspendisse sagittis tincidunt lacinia. Ut id neque dapibus, iaculis risus rhoncus, dictum ipsum. Donec eget enim mollis nunc ullamcorper tempus. Quisque auctor dapibus odio in mollis. Nam mattis lobortis nibh, nec finibus massa malesuada sed. Aenean sit amet nisi eget risus congue eleifend. Mauris dolor velit, elementum interdum tristique quis, elementum non ante.
+
+Foram gerados 5 parágrafos, 460 palavras e 3164 bytes de Lorem Ipsum</p>
+            </div><!--sobre-->
+
+            <div class='galeria'>
+
+            </div> <!--Galeria -->
+        </div><!-- conteudo -->
+
+        <div class='informes'>
+            <div class='info1'>
+
+            </div>
+
+            <div class='info2'>
+                
+            </div>
+
+            <div class='info3'>
+                
+            </div>
+
+            <div class='info4'>
+                
+            </div>
+        </div> <!--informes-->
+
+    </section><!--meio-->
+
+    <section class='final'>
+        <div class='img-futter'>
+            <h2>Apoio: </h2>
+            <div class='logo-apoio'>
+                <div class='Apoio1'>
+
+                </div>
+                <div class='Apoio2'>
+                    
+                </div>
+                <div class='Apoio3'>
+                    
+                </div>
+            </div><!--logo apoio-->
+        </div><!--futter -->
+    </section><!--final-->
 
     <script src="script/script.js"></script>
 </body>
